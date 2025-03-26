d797251b4dedcd267ba1804a77ea02cd10f06dca
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-03-26 16:16:29 +0000

    finished test_121.py, added .gitignore

---
+++ tests/test_121.py
@@ -0,0 +1,17 @@
+from leetcode_solutions.q121_best_time_to_buy_and_sell_stock import Solution
+
+def test_case_1():
+    prices = [7,1,5,3,6,4]
+    output = 5
+    solution = Solution()
+    answer = solution.solution_1(prices)
+    assert answer == output
+
+def test_case_2():
+    prices = [7,6,4,3,1]
+    output = 0
+    solution = Solution()
+    answer = solution.solution_1(prices)
+    assert answer == output
+
+
