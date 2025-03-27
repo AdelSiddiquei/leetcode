3989746a21e4acbc9e599af90bfe2730ec22d357
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-03-27 14:31:34 +0000

    finished q88.py and test_88.py

--- leetcode_solutions/q88.py
+++ leetcode_solutions/q88.py
@@ -3,10 +3,21 @@
 https://leetcode.com/problems/merge-sorted-array
 """
 
+import numpy as np
+
 
 class Solution:
+    """
+    For merging arrays lists of length m and n. The first array will have length m+n, with the final n entries as 0s.
+    The second array will have length m.
+    The output will be the merged array, sorted in ascending order.
+    """
+
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1():
-        pass
+    def solution_1(self, array1, m, array2, n):
+        for i in range(n):
+            array1[i - n] = array2[i]
+        array1.sort()
+        return array1
--- tests/test_88.py
+++ tests/test_88.py
@@ -1,6 +1,6 @@
 from leetcode_solutions.q88 import Solution
 
-test_input_1 = [[1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3]
+test_input_1 = ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
 expected_output_1 = [1, 2, 2, 3, 5, 6]
 
 test_input_2 = [[1], 1, [], 0]
@@ -12,6 +12,6 @@ expected_output_3 = [1]
 
 def test_solution_1():
     solution = Solution()
-    assert expected_output_1 == solution.solution_1(test_input_1)
-    assert expected_output_2 == solution.solution_1(test_input_2)
-    assert expected_output_3 == solution.solution_1(test_input_3)
+    assert expected_output_1 == solution.solution_1(*test_input_1)
+    assert expected_output_2 == solution.solution_1(*test_input_2)
+    assert expected_output_3 == solution.solution_1(*test_input_3)
