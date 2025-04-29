cd9367b29b3bdd4e8b1bfd2470f21600682cbf51
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-04-29 14:35:20 +0100

    improved documentation in q14.py

--- leetcode_solutions/q14.py
+++ leetcode_solutions/q14.py
@@ -8,13 +8,13 @@ class Solution:
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1(self, strings):
+    def solution_1(self, strings: list[str]) -> str:
         if not strings:
             return ""
-        prefix = strings[0]
+        prefix = strings[0]             #set prefix to first string
         for string in strings[1:]:
-            while string[: len(prefix)] != prefix:
-                prefix = prefix[:-1]
-                if not prefix:
+            while string[: len(prefix)] != prefix:   #check for match
+                prefix = prefix[:-1]               #shorten prefix for no match
+                if not prefix:                         #checking if prefix is empty
                     return ""
         return prefix
