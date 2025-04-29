36174243157827767d831368e0891cfb4d6e141e
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-04-29 16:54:30 +0100

    wrote solution and tests for q21

--- leetcode_solutions/q21.py
+++ leetcode_solutions/q21.py
@@ -8,5 +8,21 @@ class Solution:
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1():
-        pass
+    def solution_1(self, list1: list, list2: list) -> list:
+        if not list1: return list2
+        if not list2: return list1
+        merged = []
+        i = j = 0
+
+        while i < len(list1) and j < len(list2):
+            if list1[i] < list2[j]:
+                merged.append(list1[i])
+                i += 1
+            else:
+                merged.append(list2[j])
+                j += 1
+        merged.extend(list1[i:])
+        merged.extend(list2[j:])
+        return merged
+
+
--- tests/test_21.py
+++ tests/test_21.py
@@ -10,6 +10,6 @@ expected_output_3 = [0]
 
 def test_solution_1():
     solution = Solution()
-    assert expected_output_1 == solution.solution_1(test_input_1)
-    assert expected_output_2 == solution.solution_1(test_input_2)
-    assert expected_output_3 == solution.solution_1(test_input_3)
+    assert expected_output_1 == solution.solution_1(*test_input_1)
+    assert expected_output_2 == solution.solution_1(*test_input_2)
+    assert expected_output_3 == solution.solution_1(*test_input_3)
