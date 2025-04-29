3e900f8541763c22d5a50c079f071fe83b29d5d0
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-04-29 15:47:54 +0100

    formatted test_21.py

--- tests/test_21.py
+++ tests/test_21.py
@@ -1,10 +1,10 @@
 from leetcode_solutions.q21 import Solution
 
-test_input_1 = [[1,2,4],[1,3,4]]
-expected_output_1 = [1,1,2,3,4,4]
-test_input_2 = [[],[]]
+test_input_1 = [[1, 2, 4], [1, 3, 4]]
+expected_output_1 = [1, 1, 2, 3, 4, 4]
+test_input_2 = [[], []]
 expected_output_2 = []
-test_input_3 = [[],[0]]
+test_input_3 = [[], [0]]
 expected_output_3 = [0]
 
 
