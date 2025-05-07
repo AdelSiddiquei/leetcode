ee62b29e8113c1caf4bf765098ca041623bf41e1
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-07 13:07:10 +0100

    finished solution_1 to q35

--- leetcode_solutions/q35.py
+++ leetcode_solutions/q35.py
@@ -8,5 +8,8 @@ class Solution:
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1():
-        pass
+    def solution_1(self, array, target):
+        for n, x in enumerate(array):
+            if x >= target: 
+                return n                #return the index which the target fits
+        return len(array)                  #returns final index in case of no x in array >= target
