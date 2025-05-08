c26878b68f910173b63e66e8280b0381a0b21c53
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-08 10:34:15 +0100

    did solution 1 and 2 for q58

--- leetcode_solutions/q58.py
+++ leetcode_solutions/q58.py
@@ -9,5 +9,31 @@ class Solution:
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1():
-        pass
+    def solution_1(self, sentence: str) -> int:
+        """Returns the length of the final word in an input string, where a word is defined as a maximal substring containing no spaces.
+            Uses Python built in string methods.
+        Args:
+            sentence (str): input string
+
+        Returns:
+            int: Length of final word in string.
+        """
+
+        sentence = sentence.rstrip()                #remove trailing whitespace
+        sentence = sentence.split()                 #Split the string into a list of words
+
+        return len(sentence[-1])
+
+    # def solution_2(self, sentence: str) -> int:
+        
+    #     i = len(sentence) -1
+
+    #     while i >= 0 and sentence[i] == ' ':
+    #         i -= 1                              #Skipping any trailing whitespace
+        
+    #     length = 0
+    #     while i >= 0 and sentence[i] != ' ':    #Counting the letters in the word
+    #         length += 1
+    #     return length
+
+    """ Solution 2 seems to break pytest? come back"""
--- tests/test_58.py
+++ tests/test_58.py
@@ -13,3 +13,12 @@ def test_solution_1():
     assert expected_output_1 == solution.solution_1(test_input_1)
     assert expected_output_2 == solution.solution_1(test_input_2)
     assert expected_output_3 == solution.solution_1(test_input_3)
+
+
+
+def test_solution_2():
+    solution = Solution()
+    assert expected_output_1 == solution.solution_2(test_input_1)
+    assert expected_output_2 == solution.solution_2(test_input_2)
+    assert expected_output_3 == solution.solution_2(test_input_3)
+
