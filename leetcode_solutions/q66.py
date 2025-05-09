70060c6df9eb385d9a80820aee7f27db7a67626d
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-09 11:59:01 +0100

    completed solution_1 and testing for q66

--- leetcode_solutions/q66.py
+++ leetcode_solutions/q66.py
@@ -8,5 +8,19 @@ class Solution:
     def __init__(self):
         self.solution_count = 1  # update as more solutions made
 
-    def solution_1():
-        pass
+    def solution_1(self, digits: list) -> list:
+        """Adds 1 to integer number where the numbers digits are represented by list.
+
+        Args:
+            digits (list): Represents the number when read left to right.
+
+        Returns:
+            list: New list representing the original integer + 1
+        """
+        n = len(digits)
+        for i in range(1,n+1,1):        
+            if digits[-i] < 9:          #right to left traversal
+                digits[-i] += 1
+                return digits
+            digits[-i] = 0
+        return [1] + digits             #if all elements == 9 prepends a 1 to the list
